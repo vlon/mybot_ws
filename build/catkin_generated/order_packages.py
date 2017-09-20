2b7d68728f3725379ad3297a3d5bb0770c0b4a1f
hangjun zhao <alephman@gmail.com>
2017-09-20 13:27:28 +0800

    add all

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/josh/mybot_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/josh/catkin_ws_2/devel;/opt/ros/kinetic".split(';') if "/home/josh/catkin_ws_2/devel;/opt/ros/kinetic" != "" else []
